692050d74255cbc4939208fb1df6c6d7acf036ae
Harrison Rogers <harrisonrogers@Harrisons-MacBook-Pro-2.local>
2023-09-19 21:39:28 +1200

    movies pop

--- js/script.js
+++ js/script.js
@@ -2,6 +2,53 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData('movie/popular');
+
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+          <a href="movie-details.html?id=${movie.id}">
+            ${
+              movie.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="Movie Title"
+            />`
+                : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>`;
+
+    document.querySelector('#popular-movies').appendChild(div);
+  });
+}
+
+// Fetch Data from TMDB API
+async function fetchAPIData(endpoint) {
+  const API_KEY = 'c356d0134526de28726fe9fa8d2d7309';
+  const API_URL = 'https://api.themoviedb.org/3/';
+
+  const respone = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-us`
+  );
+
+  const data = await respone.json();
+
+  return data;
+}
+
 // Highlight active link
 function highlightActivelink() {
   const links = document.querySelectorAll('.nav-link');
@@ -17,7 +64,7 @@ function init() {
   switch (global.currentPage) {
     case '/':
     case '/index.html':
-      console.log('Home');
+      displayPopularMovies();
       break;
     case '/shows.html':
       console.log('Shows');
